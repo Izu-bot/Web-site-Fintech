5d5f4340a40de164729cf36c7aa03c8e6f492549
Kauan Cardoso <kauamartins977@gmail.com>
2023-07-03 16:43:00 -0300

    Atualizado

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,5 +2,5 @@
 document.querySelector('#formulario').addEventListener('submit', (e) => {
     e.preventDefault();
 
-    window.location.href = '/assets/pages/home.html';
+    window.location.href = '../assets/pages/home.html';
 })
